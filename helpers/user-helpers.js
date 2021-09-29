7594f9c83958aa4cb1e140e63345f2adc9e5283a
Vishnu S <vs12339@gmail.com>
2021-09-29 11:37:38 +0530

    place-order added day5

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -156,7 +156,7 @@ module.exports ={
                 $inc:{'products.$.quantity':details.count}
             }
             ).then((response)=>{
-                resolve(true)
+                resolve({status:true})
             })
           }
        }) 
@@ -223,6 +223,39 @@ module.exports ={
         ]).toArray()
         // console.log(total[0].total)
         resolve(total[0].total)
-    })
-}
-}
+     })
+  },
+   placeOrder:(order,products,total)=>{
+       return new Promise((resolve,reject)=>{
+           console.log(order,products,total);
+           let status = order['payment-method']==='COD'?'placed':'pending'
+           let orderObj={
+               deliveryDetails:{
+                   mobile:order.mobile,
+                   address:order.address,
+                   pincode:order.pincode
+               },
+               userId:objectId(order.userId),
+               paymentMethod:order['payment-method'],
+               products:products,
+               totalAmount:total,
+               status:status,
+               date: new Date()
+
+           }
+
+           db.get().collection(collection.ORDER_COLLECTION).insertOne(orderObj).then((response)=>{
+               db.get().collection(collection.CART_COLLECTION).deleteOne({user:objectId(order.userId)})
+               resolve()
+           })
+       })
+   },
+   getCartProductList:(userId)=>{
+       return new Promise(async(resolve,reject)=>{
+
+           let cart = await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
+           resolve(cart.products)
+       })
+   }
+
+   }
--- routes/user.js
+++ routes/user.js
@@ -63,10 +63,10 @@ router.get('/logout',(req,res)=>{
   res.redirect('/')
 })
 router.get('/cart',verifyLogin,async(req,res)=>{
-  let total=await userHelpers.getTotalAmount(req.session.user._id)
+  let totalValue=await userHelpers.getTotalAmount(req.session.user._id)
   let products = await userHelpers.getCartProducts(req.session.user._id,)
   console.log(products)
-  res.render("user/cart",{products,user:req.session.user,total})
+  res.render("user/cart",{products,user:req.session.user._id,totalValue})
 })
 
 router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{
@@ -76,7 +76,8 @@ router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{
   })
 })
 router.post('/change-product-quantity',(req,res,next)=>{
-  userHelpers.changeProductQuantity(req.body).then((response)=>{
+  userHelpers.changeProductQuantity(req.body).then(async(response)=>{
+    response.total = await userHelpers.getTotalAmount(req.body.user)
     res.json(response)
 
   })
@@ -92,5 +93,14 @@ router.get('/place-order',verifyLogin, async(req,res)=>{
   let total=await userHelpers.getTotalAmount(req.session.user._id)
   res.render('user/place-order',{total,user:req.session.user});
 
+})
+router.post('/place-order',async(req,res)=>{
+  let products = await userHelpers.getCartProductList(req.body.userId)
+  let totalPrice = await userHelpers.getTotalAmount(req.body.userId)
+  userHelpers.placeOrder(req.body,products,totalPrice).then((response)=>{
+    res.json({status:true})
+
+  })
+  console.log(req.body);
 })
 module.exports = router;
